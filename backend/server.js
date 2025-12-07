1c643d7b1c7b49343b93181ea3ad0b7fe5595a43
dqvuong2111 <dqvuong2111@gmail.com>
2025-12-07 13:02:26 +0700

    Create logic process for login/register

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRoute from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config';
+
 // app config
 const app = express();
 const port = 4000;
@@ -16,6 +19,8 @@ connectDB();
 //api endpoints
 app.use("/api/food", foodRoute);
 app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+
 app.get("/", (req, res) => {
   res.send("API Working hehehehe");
 });
